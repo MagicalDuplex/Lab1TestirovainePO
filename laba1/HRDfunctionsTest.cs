dcf03b60af88593af142c8d9b5f8bd4314db19f3
Евгений Сапожников <jenya.sapozhnickov@yandex.ru>
2020-11-22 19:40:35 +0300

    Добавьте файлы проекта.

---
+++ laba1/HRDfunctionsTest.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace laba1
+{
+    [TestFixture]
+    class HRDfunctionsTest
+    {
+        HumanResourcesDepartmentFunctions hrdFunctions;
+        [SetUp]
+        public void SetUp()
+        {
+            hrdFunctions = new HumanResourcesDepartmentFunctions();
+        }
+
+        [Test]
+        public void DataAboutWorkerAddTest()
+        {
+            string name = "Имя";
+            string secondname = "Фамилия";
+            string age = "25";
+            string monthsWorkedOut = "7";
+            string[] dataAboutWorker = { "Имя","Фамилия","252222","7"};
+            Assert.AreNotEqual(dataAboutWorker, hrdFunctions.DataAboutWorkerAdd(name, secondname, age, monthsWorkedOut));
+        }
+
+        [Test]
+        public void ExistPersonTest()
+        {
+            long pasportSeriesAndNumber = 2727899067;
+            List<long> bdWorkers = new List<long>() { 2729898212, 2927785656, 2727899067 };
+            Assert.IsTrue(hrdFunctions.ExistPerson(pasportSeriesAndNumber, bdWorkers));
+        }
+
+        [Test]
+        public void SalaryForDayTest()
+        {
+            int hours = 8;
+            int priceForHour = 100;
+            int position = 3;
+            int salary = 1112;
+            Assert.AreEqual(salary, hrdFunctions.SalaryForDay(hours, priceForHour, position));
+        }
+
+        [Test]
+        public void UpdateWorkedoutHoursesTest()
+        {
+            int LastWorkedTime = 26;
+            int AddWorkedTime = 8;
+            int UpdateWorkedTime = 34;
+            Assert.AreEqual(UpdateWorkedTime, hrdFunctions.UpdateWorkedoutHourses(LastWorkedTime, AddWorkedTime));
+        }
+
+        [Test]
+        public void DataAboutSelectedWorkerTest()
+        {
+            int id = 1;
+            string[][] bdWorkers = new string[3][];
+            bdWorkers[0] = new string[4] { "Имя1","Фамилия1","24","7"};
+            bdWorkers[1] = new string[4] { "Имя2", "Фамилия2", "31", "12" };
+            bdWorkers[2] = new string[4] { "Имя3", "Фамилия3", "26", "6" };
+            string data = "Имя = Имя2, Фамилия = Фамилия2, Возраст = 31, Число отработанных месяцев = 12.";
+            Assert.AreEqual(data, hrdFunctions.DataAboutSelectedWorker(id, bdWorkers));
+        }
+    }
+}
---
+++ laba1/HumanResourcesDepartmentFunctions.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace laba1
+{
+    class HumanResourcesDepartmentFunctions
+    {
+
+        // Метод, заносящий данные о работнике в массив
+        public string[] DataAboutWorkerAdd(string name,string secondname, string age, string monthsWorkedOut)
+        {
+            string[] dataAboutWorker = new string[4];
+            dataAboutWorker[0] = name;
+            dataAboutWorker[1] = secondname;
+            dataAboutWorker[2] = age;
+            dataAboutWorker[3] = monthsWorkedOut;
+
+            return dataAboutWorker;
+        }
+
+        // Данный метод проверяет существование человека по паспорту в БД рабочих
+        public bool ExistPerson(long pasportSeriesAndNumber, List<long> bdWorkers)
+        {
+            if (bdWorkers.Contains(pasportSeriesAndNumber))
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        // Метод, считающий заработную плату за день
+        // position - должностной ранг
+        public int SalaryForDay(int hours,int priceForHour,int position)
+        {
+            int salary = 0;
+            switch (position)
+            {
+                case 1:
+                    salary = hours * priceForHour;
+                    break;
+                case 2:
+                    salary = (int)(hours * priceForHour * 1.28);
+                    break;
+                case 3:
+                    salary = (int)(hours * priceForHour * 1.39);
+                    break;
+                case 4:
+                    salary = (int)(hours * priceForHour * 1.6);
+                    break;
+                default:
+                    salary = 0;
+                    break;
+            }
+            return salary;
+        }
+
+        // Метод, осуществляющий обновление учтенного отработанного врмени
+        public int UpdateWorkedoutHourses(int LastWorkedTime, int AddWorkedTime)
+        {
+            int UpdateWorkedTime = LastWorkedTime + AddWorkedTime;
+            return UpdateWorkedTime;
+        }
+
+        // Метод осуществляющий вывод данных о рабочем
+        public string DataAboutSelectedWorker(int id, string[][] bdWorkers)
+        {
+            string data = "Имя = " + bdWorkers[id][0] +
+                ", Фамилия = "+ bdWorkers[id][1] +
+                ", Возраст = "+ bdWorkers[id][2] +
+                ", Число отработанных месяцев = "+bdWorkers[id][3]+".";
+            return data;
+        }
+    }
+}
